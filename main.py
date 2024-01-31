0524bb5ec7e1381901899a3e00af47ed6e7cdb30
Serpyne <156378996+Serpyne@users.noreply.github.com>
2024-02-01 00:57:19 +1100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.server import Server
 intents = discord.Intents.all()
 
 app = Server(__name__)
-app.start(host='0.0.0.0', port=8000, debug=False)
+app.start(host='0.0.0.0', port=8080)
 
 study_bot = StudyBot(command_prefix=".",
                      case_insensitive=True, intents=intents)
